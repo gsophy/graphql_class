c08ab2fa87c4b98f80523f2f4d1eefafe781ddcb
George Sophy <georgesophy@gmail.com>
2021-04-02 13:49:46 -0400

    Chapter 2 changes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import schema from './schema';
+import { graphqlHTTP }  from 'express-graphql';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send ('GraphQL is amazing');
 });
+
+const root = { hello: () => "Hi I'm Whorrey"};
+
+app.use ('/graphql', graphqlHTTP ({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(8080, () => console.log("The server is running on port localhost:8080/graphql"));
